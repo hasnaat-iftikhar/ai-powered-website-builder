53c02bd7164f6a6f0e4fb512b68a163ba1fda6df
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2025-11-01 18:00:18 +0500

    feat - bg jobs: worked on implementing inngest for background jobs inside the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
 export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
   return (
-    <div></div>
+    <button className="bg-red-700" onClick={() => invoke.mutate({ text: "Hello World" })}>
+      Invoke Jobs
+    </button>
   );
 }
