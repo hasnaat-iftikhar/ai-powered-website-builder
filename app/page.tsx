d707f9f0ae77aaaeec0a8218a71623a7054bb381
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2025-11-02 00:33:03 +0500

    feat - bg jobs: worked on implementing agent ai via ingest in app

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,25 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({}));
 
   return (
-    <button className="bg-red-700" onClick={() => invoke.mutate({ text: "Hello World" })}>
+    <>
+    <input
+      className="border-2 border-black"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+
+    <button className="bg-red-700" onClick={() => invoke.mutate({ value })}>
       Invoke Jobs
     </button>
+    </>
   );
 }
--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,10 +1,18 @@
 import { inngest } from "./client";
+import { Agent, createAgent, openai } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
 	{ id: "hello-world" },
 	{ event: "test/hello.world" },
-	async ({ event, step }) => {
-		await step.sleep("wait-a-moment", "1s");
-		return { message: `Hello ${event.data.email}!` };
+	async ({ event }) => {
+		const componentBuilder = createAgent({
+			name: "Component Builder",
+			system: "You are a text component builder. You take user input and return a structured component in react js.",
+			model: openai({ model: "gpt-4o" })
+		});
+
+		const { output } = await componentBuilder.run(`Write the following react component for me: ${event.data.value}`);
+
+		return { output };
 	}
 );
